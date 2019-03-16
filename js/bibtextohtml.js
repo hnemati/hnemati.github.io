cbf440b810375911bfdc138012cead078aea29b0
Hamed Nemati <hnnemati@kth.se>
2019-03-16 18:16:09 +0100

    fixing xmlreq

--- js/bibtextohtml.js
+++ js/bibtextohtml.js
@@ -445,6 +445,48 @@ function bibtex_js_draw() {
   bibtexShow($("#bibtex_input").val(), $("#bibtex_display"));
 }
 
+function doCORSRequest(options, printResult) {
+  
+  if (window.XMLHttpRequest) {
+      // code for IE7+, Firefox, Chrome, Opera, Safari
+      x = new XMLHttpRequest();
+  }
+  else {
+      // code for IE6, IE5
+      x = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  var overlay = new ItpOverlay();
+
+  x.open(options.method, options.url, true);
+  x.onload = x.onerror = function() {
+  printResult(
+    (x.responseText || '')
+   );
+  };
+  
+  x.onloadstart = function(e) {
+    overlay.show("body");
+  };
+  
+  x.onloadend = function(e) {
+    overlay.hide("body");
+    bibtex_js_draw();
+  };
+  
+  x.send(options.data);
+}
+
+function loadFile() {
+// Bind event
+  var outputField = document.getElementById('bibtex_input');
+  doCORSRequest({
+   method: 'GET',
+   url: 'https://raw.githubusercontent.com/hnemati/hnemati.github.io/master/biblio.bib',
+  }, function printResult(result) {
+       outputField.value = result;
+     })
+};
+
 function loadBib() {
   var xmlhttp;
   var overlay = new ItpOverlay();
@@ -485,6 +527,6 @@ if (typeof jQuery == 'undefined') {
     if ($(".bibtex_template").size() == 0) {
       $("body").append("<span class=\"tag\"></span><div class=\"bibtex_template\"><div class=\"if author\" style=\"font-weight: bold;\">\n  <span class=\"if year\">\n    <span class=\"year\"></span>, \n  </span>\n  <span class=\"author\"></span>\n  <span class=\"abstract\"></span>\n  <span class=\"if url\" style=\"margin-left: 20px\">\n    <a class=\"url\" style=\"color:black; font-size:10px\">(view online)</a>\n  </span>\n</div>\n<div style=\"margin-left: 10px; margin-bottom:5px;\">\n  <span class=\"title\"></span>\n</div></div>");
     };
-    loadBib();
+    loadFile();
   });
 }
